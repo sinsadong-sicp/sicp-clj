3c2bb90d91ed4cd35fd3e0e236a12c82988af8fd
Hwidong Bae <spilist@gmail.com>
2014-02-09 18:29:48 +0900

    시간이 없어서 일단 연습문제 1-15까지 푼 것 올림

--- src/sicp_clj/ch1/hd.clj
+++ src/sicp_clj/ch1/hd.clj
@@ -57,3 +57,120 @@
         guess
         (sqrt-iter (improve guess x))))
   (sqrt-iter 1.0))
+
+; --- 2주차 (~2월 9일) ---
+(defn factorial-pure[n] ;정상작동
+	(if (= n 1) 1 (* n (factorial-pure (- n 1)))))
+
+(defn factorial-iter[n] ;integer overflow 발생
+	(defn iter[product counter]
+		(if > counter n)
+			product
+			(iter (* counter product) (+ counter 1)))
+	(iter 1 1))
+
+(defn factorial-reduce[n]
+	(reduce * (range 1 (inc n))))
+
+; 1-9
+; inc 먼저: resursive
+; (+ 4 5)
+; (inc (+ 3 5))
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+
+; dec 먼저: interative
+; (+ 4 5)
+; (+ (dec 4) (inc 5))
+; (+ 3 6)
+; (+ (dec 3) (inc 6))
+; (+ 2 7)
+; (+ (dec 2) (inc 7))
+; (+ 1 8)
+; (+ (dec 1) (inc 8))
+; (+ 0 9)
+; 9
+
+; 1-10
+(defn acnn [x y]
+	(cond
+		(= y 0) 0
+		(= x 0) (* 2 y)
+		(= y 1) 2
+		:else (acnn (dec x) (acnn x (dec y))))
+	)
+
+;a 1 10 = 1024
+
+;a 2 4 = 65536
+;a 1 (a 2 3)
+;a 1 (a 1 (a 2 2))
+;a 1 (a 1 (a 1 (a 2 1)))
+;a 1 (a 1 (a 1 2))
+;a 1 (a 1 4)
+;a 1 16
+;65536
+
+;a 3 3 = 65536
+;a 2 (a 3 2)
+;a 2 (a 2 (a 3 1))
+;a 2 (a 2 2)
+;a 2 (a 1 (a 2 1))
+;a 2 (a 1 2)
+;a 2 4
+;65536
+
+; (defn acnn-f[n] (acnn 0 n)) => 2n
+; (defn acnn-g[n] (acnn 1 n)) => 2^n
+; (defn acnn-h[n] (acnn 2 n)) => 2^2^ ... (n번) => 수학적으로 어떻게 표시? 
+; 양의 정수 n에 대해 h(n) = 2 when n is 1, 2^{h(n-1)} others 이렇게 하면 되나.
+
+; 1-11
+(defn fib[n]
+	(defn iter[a b count]
+		(if (= count 0) 
+			b 
+			(iter (+ a b) a (dec count))))
+	(iter 1 0 n))
+
+(defn ex11-rec[n]
+	(cond 
+		(< n 3) n
+		:else (+ 
+			(ex11-rec (- n 1))
+			(* 2 (ex11-rec (- n 2)))
+			(* 3 (ex11-rec (- n 3))))
+		))
+
+(defn ex11-iter[n]
+	(defn iter[a b c count]
+		(if (= count 0)
+			c
+			(iter (+ a (* 2 b) (* 3 c)) a b (dec count))))
+	(iter 2 1 0 n))
+
+; 1-12
+(defn pascal-tri[n]	
+	(defn pascal-val[level idx]
+		(cond			
+			(= idx 0) 1
+			(= idx level) 1
+			:else (+ (nth (pascal-tri (dec level)) (dec idx)) (nth (pascal-tri (dec level)) idx))
+			))
+	(into [] (for [x (range 0 (inc n))] (pascal-val n x))))
+
+; 1-13 => pass
+
+; 1-14 => pass
+
+; 1-15
+(defn pow[n, x]
+	(if (= x 0) 1 (reduce * (take x (repeat n)))))
+; a. 3^4=81, 3^5=243. 3의 x승이 121.5을 넘기면 종료하는 것이니까 5번.
+; b. (sine a)는 a를 0.1로 나눈 값이 3의 x승을 넘길 때까지 계산. 즉 ceiling(log (10a) / (log 3)) 번 한다. 자람 차수는 (log a).
