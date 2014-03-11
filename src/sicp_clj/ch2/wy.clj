1d04e6a39b9943b662d35dbe73aa1c03ffeb62ff
Woongcheol <woongcheol@gmail.com>
2014-03-11 10:35:35 +0900

    Solve from 2-3 to 2-6

--- src/sicp_clj/ch2/wy.clj
+++ src/sicp_clj/ch2/wy.clj
@@ -1,5 +1,5 @@
 (ns sicp-clj.ch2.wy
-  (:use [clojure.contrib.math :only [abs gcd]])
+  (:use [clojure.contrib.math :only [abs expt gcd sqrt]])
   (:use [clojure.contrib.generic.math-functions :only [sgn]]))
 
 ; 2-1
@@ -33,3 +33,88 @@
         x (average (x-point p) (x-point q))
         y (average (y-point p) (y-point q))]
     (make-point x y)))
+
+; 2-3
+
+(defn length-of-segment [s]
+  (let [p (start-of-segment s)
+        q (end-of-segment s)]
+    (sqrt
+      (+
+        (expt (- (x-point p) (x-point q)) 2)
+        (expt (- (y-point p) (y-point q)) 2)))))
+
+; bottom left point + top right point
+(defn make-rectangle [p q]
+  [p q])
+(defn length-of-rectangle [r]
+  (let [p (first r)
+        q (second r)]
+    (length-of-segment
+      (make-segment
+        p
+        (make-point (x-point q) (y-point p))))))
+(defn width-of-rectangle [r]
+  (let [p (first r)
+        q (second r)]
+    (length-of-segment
+      (make-segment
+        p
+        (make-point (x-point p) (y-point q))))))
+
+(defn perimeter-of-rectangle [r]
+  (* 2 (+ (length-of-rectangle r) (width-of-rectangle r))))
+
+(defn area-of-rectangle [r]
+  (* (length-of-rectangle r) (width-of-rectangle r)))
+
+; 2-4
+
+; (car (cons x y))
+; => ((cons x y) (fn [p q] p))
+; => ((fn [m] (m x y)) (fn [p q] p))
+; => ((fn [p q] p) x y)
+; => x
+
+(defn cdr [z]
+  (z (fn [p q] q)))
+
+; 2-5
+
+(defn my-cons [a b]
+  (* (expt 2 a) (expt 3 b)))
+
+(defn my-car [n]
+  (if (odd? n)
+    0
+    (inc (my-car (/ n 2)))))
+
+(defn my-cdr [n]
+  (if (not (zero? (rem n 3)))
+    0
+    (inc (my-cdr (/ n 3)))))
+
+; 2-6
+
+(def zero
+  (fn [f]
+    (fn [x]
+      x)))
+
+(defn add-1 [n]
+  (fn [f]
+    (fn [x]
+      (f ((n f) x)))))
+
+(def one
+  (fn [f]
+    (fn [x]
+      (f x))))
+
+(def two
+  (fn [f]
+    (fn [x]
+      (f (f x)))))
+
+(defn add [m n]
+  (fn [f] (comp (m f) (n f))))
