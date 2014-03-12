f116a2b36db053bb531b60f40ace27b90268e6de
Hwidong Bae <spilist@gmail.com>
2014-03-12 13:48:40 +0900

    웅철 것 보고 2-1 간단하게 고침

--- src/sicp_clj/ch2/hd/01.clj
+++ src/sicp_clj/ch2/hd/01.clj
@@ -1,10 +1,6 @@
-(ns sicp-clj.ch1.hd
-  (:use clojure.contrib.generic.math-functions))
-
-(defn gcd[a b]
-  (if (zero? b)
-    a
-    (gcd b (rem a b))))
+(ns sicp-clj.ch2.hd
+  (:use [clojure.contrib.math :only [abs gcd]])
+  (:use [clojure.contrib.generic.math-functions :only [sgn]]))
 
 ; (defn make-rat [n d]
 ;   (let [g (gcd n d)]
@@ -20,9 +16,6 @@
 
 ; 2-1
 (defn make-rat [n d]
-  (defn make-neg [x]
-    (if (neg? x) x (- x)))
-  (let [g (gcd n d)]
-    (if (pos? (* n d))
-      [(abs (/ n g)) (abs (/ d g))]
-      [(make-neg (/ n g)) (abs (/ d g))])))
+  (let [g (gcd n d)
+    sign (* (sgn n) (sgn d))]
+    [(* sign (abs (/ n g))) (abs (/ d g))] ))
