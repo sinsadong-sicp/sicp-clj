e840ab0399b78dac1c967dc599fefbc7775e1ef8
Hoyoon Jun <zeratul9901@gmail.com>
2014-03-23 23:31:56 +0900

    이왕 고친김에 2-1도 cons로 고침

--- src/sicp_clj/ch2/hy/hy.clj
+++ src/sicp_clj/ch2/hy/hy.clj
@@ -20,8 +20,8 @@ dispatch)
 (defn make-rat [n d]
 	(let [g (gcd n d)]
 		(if (or (and (>= n 0) (> d 0)) (and (< n 0) (< d 0)))
-			(vector (/ (abs n) g) (/ (abs d) g))
-			(vector (- (/ (abs n) g)) (/ (abs d) g))
+			(cons (/ (abs n) g) (/ (abs d) g))
+			(cons (- (/ (abs n) g)) (/ (abs d) g))
 			)))
 
 ;2-2
--- src/sicp_clj/ch2/hy/hytest.clj
+++ src/sicp_clj/ch2/hy/hytest.clj
@@ -3,6 +3,6 @@
 	(:use [sicp-clj.ch1.hy.hytest :only [hytest hytest-with-error]]))
 (println "Chapter 2")
 ;2-1
-(hytest "2-1" 2 (first (make-rat 4 6)))
+(hytest "2-1" 2 (car (make-rat 4 6)))
 ;2-2
 (print-point (midpoint-segment (make-segment (make-point 0 0 ) (make-point 1 1))))
