2b144c8c7604b81e8f9c7e6b48f4ca212bf7595a
Hwidong Bae <spilist@gmail.com>
2014-04-24 23:13:49 +0900

    2-77 품

---
+++ src/sicp_clj/ch2/hd/77.clj
@@ -0,0 +1,9 @@
+(ns sicp-clj.ch2.hd
+    (:use [clojure.contrib.math :only []])
+    (:use [clojure.contrib.generic.math-functions :only []]))
+
+; 2-77
+; (magnitude z)
+; -> complex 태그의 magnitude를 dispatch
+; -> 이것을 rectangular tag의 magnitude로 dispatch
+; 2번? 아 잘 모르겠다. 계속 헷갈린다. 한글로 처음부터 다시 읽어봐야겠다. 일단 커밋.
