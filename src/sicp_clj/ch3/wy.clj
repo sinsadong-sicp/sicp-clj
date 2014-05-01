c1fd92ede8236f8b3ecea60155e7707d250ae4ee
Woongcheol <woongcheol@gmail.com>
2014-05-01 22:16:04 +0900

    Solve 3-3

--- src/sicp_clj/ch3/wy.clj
+++ src/sicp_clj/ch3/wy.clj
@@ -18,3 +18,22 @@
         :else (do
                 (swap! counter + 1)
                 (f x))))))
+
+; 3-3
+
+(defn make-account [initial-balance password]
+  (let [balance (atom initial-balance)]
+    (defn withdraw [amount]
+      (if (>= @balance amount)
+        (swap! balance - amount)
+        "Insufficient funds"))
+    (defn deposit [amount]
+      (swap! balance + amount))
+    (defn dispatch [p m]
+      (if (= p password)
+        (cond
+          (= m 'withdraw) withdraw
+          (= m 'deposit) deposit
+          :else (throw (Exception. "Unknown request -- MAKE-ACCOUNT")))
+        (fn [_] "Incorrect password")))
+    dispatch))
