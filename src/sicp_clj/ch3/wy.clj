453f43d37971fdb737daec731849a2ede390f822
Woongcheol <woongcheol@gmail.com>
2014-05-04 15:11:54 +0900

    Solve 3-9, 3-10, 3-11

--- src/sicp_clj/ch3/wy.clj
+++ src/sicp_clj/ch3/wy.clj
@@ -99,3 +99,61 @@
 ; => (+ (f 0) 1)
 ; => (+ 0 1)
 ; => 1
+
+; 3-9
+
+; recursive
+; global <- E1 (n: 6) [body of factorial]
+; global <- E2 (n: 5) [body of factorial]
+; global <- E3 (n: 4) [body of factorial]
+; global <- E4 (n: 3) [body of factorial]
+; global <- E5 (n: 2) [body of factorial]
+; global <- E6 (n: 1) [body of factorial]
+
+; iterative
+; global <- E1 (n: 6) [body of factorial]
+; global <- E2 (product: 1, counter: 1, max-count: 6) [body of iter]
+; global <- E3 (product: 1, counter: 2, max-count: 6) [body of iter]
+; global <- E4 (product: 2, counter: 3, max-count: 6) [body of iter]
+; global <- E5 (product: 6, counter: 4, max-count: 6) [body of iter]
+; global <- E6 (product: 24, counter: 5, max-count: 6) [body of iter]
+; global <- E7 (product: 120, counter: 6, max-count: 6) [body of iter]
+; global <- E8 (product: 720, counter: 7, max-count: 6) [body of iter]
+
+; 3-10
+
+; (def W1 (make-withdraw 100))
+; global <- E1 (initial-amount: 100) [body of make-withdraw]
+; E1 <- E2 (balance: 100) [body of lambda]
+
+; (W1 50)
+; global <- E1 (initial-amount: 100) [body of make-withdraw]
+; E1 <- E2 (balance: 50) [body of lambda]
+
+; (def W2 (make-withdraw 100))
+; global <- E1 (initial-amount: 100) [body of make-withdraw]
+; E1 <- E2 (balance: 50) [body of lambda]
+; global <- E3 (initial-amount: 100) [body of make-withdraw]
+; E3 <- E4 (balance: 100) [body of lambda]
+
+; 3-11
+
+; (def acc (make-account 50))
+; global <- E1 (balance: 50, withdraw: ..., deposit: ..., dispatch: ...) [body of make-account]
+
+; ((acc 'deposit) 40)
+; global <- E1 (balance: 50, withdraw: ..., deposit: ..., dispatch: ...) [body of make-account]
+; E1 <- E2 (m: 'deposit) [body of dispatch]
+; E1 <- E3 (amount: 40) [body of deposit]
+
+; ((acc 'withdraw) 60)
+; global <- E1 (balance: 90, withdraw: ..., deposit: ..., dispatch: ...) [body of make-account]
+; E1 <- E4 (m: 'withdraw) [body of dispatch]
+; E1 <- E5 (amount: 60) [body of withdraw]
+
+; local state for acc is kept in E1
+
+; (def acc2 (make-account 100))
+; global <- E6 (balance: 100, withdraw: ..., deposit: ..., dispatch: ...) [body of make-account]
+
+; local state for acc2 is kept separately in E6
