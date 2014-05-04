ee984e1a84cc57562eefb94bb9cf67b3b6eb2536
Hoyoon Jun <zeratul9901@gmail.com>
2014-05-04 23:51:12 +0900

    3-5, 3-7, 3-8

--- src/sicp_clj/ch3/hy/hy.clj
+++ src/sicp_clj/ch3/hy/hy.clj
@@ -45,16 +45,56 @@
         (fn [x] "Incorrect password")
         ))
     (defn dispatch [pw m] ;3-4
+      (defn increase-bad [x]
+        (do
+          (swap! attempt inc)
+          (if (>= @attempt 2)
+            (call-the-cop)
+            "Incorrect password")))
       (if (= pw @passwd)
         (cond
           (= m 'withdraw) withdraw
           (= m 'deposit) deposit
+          (= m 'bad-passwd) increase-bad
           :else (throw (Exception. "Unknown request - MAKE-ACCOUNT" m)))
-        (fn [x]
-          (do
-            (swap! attempt inc)
-            (if (>= @attempt 2)
-              (call-the-cop)
-              "Incorrect password")))
+        increase-bad
         ))
   dispatch))
+
+;3-5
+(defn monte-carlo [trials experiment]
+  (defn iter [trials-remaining trials-passed]
+    (cond
+      (= trials-remaining 0) (/ trials-passed trials)
+      (experiment) (iter (dec trials-remaining) (inc trials-passed))
+      :else (iter (dec trials-remaining) trials-passed)))
+  (iter trials 0))
+(defn random-in-range [low high]
+  (let [range (- high low)]
+    (+ low (rand range))))
+(defn estimate-integral [p x1 x2 y1 y2 trials]
+  (*
+    (* (- x2 x1) (- y2 y1))
+    (monte-carlo
+      trials
+      (fn []
+        (p (random-in-range x1 x2)
+           (random-in-range y1 y2))
+      )
+    )))
+
+;3-7
+(defn make-joint [account oldpasswd newpasswd]
+  (fn [pass m]
+    (if (= pass newpasswd)
+      (account oldpasswd m)
+      (account newpasswd 'bad-passwd))))
+
+;3-8
+(defn f1 [value1]
+  (defn f2 [value2]
+    (let [value3 (atom value1)]
+      (swap! value3 = value2)
+      @value3))
+  f2)
+(defn f [] (f1 0))
--- src/sicp_clj/ch3/hy/hytest.clj
+++ src/sicp_clj/ch3/hy/hytest.clj
@@ -21,4 +21,22 @@
 (hytest "3-3" "Incorrect password" ((C 'some-other-password 'withdraw) 50))
 
 ;3-4
-(hytest "3-3" "Police officer" ((C 'some-other-password 'withdraw) 50))
+(hytest "3-4" "Police officer" ((C 'some-other-password 'withdraw) 50))
+
+;3-5
+(hytest-with-error "3-5" 3.14 (* 4.0 (estimate-integral
+    (fn [x y]
+      (if (<= (+ (* x x) (* y y)) 1)
+        true
+        false
+      )) 0 1 0 1 6000))) ;10000하니까 stackoverflow난다 왜지;; 내 코드가 비효율적으로 짜져있는듯
+
+;3-7
+(def D (make-account 100 'secret-password))
+(def D2 (make-joint D 'secret-password 'new-password))
+(def D3 (make-joint D 'some-password 'secret-password))
+(hytest "3-7" 60 ((D 'secret-password 'withdraw) 40))
+(hytest "3-7" 20 ((D2 'new-password 'withdraw) 40))
+(hytest "3-7" "Incorrect password" ((D2 'some-password 'withdraw) 40))
+(hytest "3-7" "Police officer" ((D3 'secret-password 'withdraw) 40))
+
