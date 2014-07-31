545c7b8ca547041a0a06068767f8e3e8ce9d24a0
Woongcheol Yang <woongcheol@gmail.com>
2014-08-01 00:05:11 +0900

    Solve 4-4

--- src/sicp_clj/ch4/wy.clj
+++ src/sicp_clj/ch4/wy.clj
@@ -2,14 +2,30 @@
 
 ; 4-1
 
-(defn list-of-values-ltr [exps env]
-  (if (no-operands? exps)
+(defn list-of-values-ltr [exp env]
+  (if (no-operands? exp)
     nil
-    (let [first-value (eval (first-operand exps) env)]
-      (cons first-value (list-of-values-ltr (rest-operands exps) env)))))
+    (let [first-value (eval (first-operand exp) env)]
+      (cons first-value (list-of-values-ltr (rest-operands exp) env)))))
 
-(defn list-of-values-rtl [exps env]
-  (if (no-operands? exps)
+(defn list-of-values-rtl [exp env]
+  (if (no-operands? exp)
     nil
-    (let [rest-values (list-of-values-rtl (rest-operands exps) env)]
-      (cons (eval (first-operand exps) env) rest-values))))
+    (let [rest-values (list-of-values-rtl (rest-operands exp) env)]
+      (cons (eval (first-operand exp) env) rest-values))))
+
+; 4-4
+
+(defn eval-and [exp env]
+  (if (no-operands? exp)
+    true
+    (if (false? (eval (first-operand exp) env))
+      false
+      (eval-and (eval (rest-operands exp) env)))))
+
+(defn eval-or [exp env]
+  (if (no-operands? exp)
+    false
+    (if (true? (eval (first-operand exp) env))
+      true
+      (eval-or (eval (rest-operands exp) env)))))
